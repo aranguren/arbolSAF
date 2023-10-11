e1ffebe23951b5c9a0e3663dd122e6e62bf3692c
Ernesto Ruiz <ernesto930526@gmail.com>
2023-10-11 15:24:46 -0400

    arreglando impesion del pdf

--- apps/static/assets/js/tool_arbolsaf.js
+++ apps/static/assets/js/tool_arbolsaf.js
@@ -1162,11 +1162,13 @@ $(document).ready(function() {
                 responseType: 'blob'
             },
             success: function(blob) {
-                console.log(blob.size);
-                var link=document.createElement('a');
-                link.href=window.URL.createObjectURL(blob);
-                // link.target = "_blank";
-                // link.download="PDFname_" + new Date() + ".pdf";
+                var link = downloadBlob(blob, "Reporte-ArbolSAF.pdf");
+                console.log("generando link con nombre")
+                //console.log(blob.size);
+                //var link=document.createElement('a');
+                //link.href=window.URL.createObjectURL(blob);
+                //link.target = "_blank";
+                //link.download="Reporte_" + new Date() + ".pdf";
                 link.click();
             },
             error: function( jqXHR, textStatus, errorThrown ) {
@@ -1174,6 +1176,45 @@ $(document).ready(function() {
             }
         });
     })
+
+    function downloadBlob(blob, filename) {
+        // Create an object URL for the blob object
+        const url = URL.createObjectURL(blob);
+      
+        // Create a new anchor element
+        const a = document.createElement('a');
+      
+        // Set the href and download attributes for the anchor element
+        // You can optionally set other attributes like `title`, etc
+        // Especially, if the anchor element will be attached to the DOM
+        a.href = url;
+        a.download = filename || 'download';
+        a.target = "_blank"
+      
+        // Click handler that releases the object URL after the element has been clicked
+        // This is required for one-off downloads of the blob content
+        const clickHandler = () => {
+          setTimeout(() => {
+            URL.revokeObjectURL(url);
+            removeEventListener('click', clickHandler);
+          }, 150);
+        };
+      
+        // Add the click event listener on the anchor element
+        // Comment out this line if you don't want a one-off download of the blob content
+        a.addEventListener('click', clickHandler, false);
+      
+        // Programmatically trigger a click on the anchor element
+        // Useful if you want the download to happen automatically
+        // Without attaching the anchor element to the DOM
+        // Comment out this line if you don't want an automatic download of the blob content
+        //a.click();
+      
+        // Return the anchor element
+        // Useful if you want a reference to the element
+        // in order to attach it to the DOM or use it in some other way
+        return a;
+      }
 
     $(document).on("click", "#next-button", function () {
         // $("body").scrollTop();
