99980ebfb86c0a7b73b859c8a628aa2d8c11ec26
Ernesto Ruiz <ernesto930526@gmail.com>
2023-12-04 16:42:36 -0500

    aumentando permisos para la exportacion en la tabla cruzada

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ from django.contrib import admin
 from django.urls import path, include  # add this
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
@@ -21,3 +22,5 @@ handler403 = 'core.error_views.render_403_view'
 handler400 = 'core.error_views.render_400_view'
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_url = '/arbolsaf/especie/listado'  # Removes the 'View Site' link
